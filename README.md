a8d51ae47f7bf807dac19d83aabc371177813860
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-03-25 15:48:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+#  运行 junit 报错： no task available 
+
+    进入gradle 设置页面
+        Build ---> Gradle ---> Build and runn using 和 Run test using 都选择 Intelij IEAD 就可以了
+    
+    这是IDEA版本bug 2019.1.1 就有这个问题  2019.2.4之后，这个bug就好了，即使是默认的，也能正常运行junit test 了
+
---
+++ chapter0-0-8/src/main/java/com/goat/chapter009/App.java
@@ -0,0 +1,19 @@
+package com.goat.chapter009;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2020/3/25.
+ *
+ * @ Description: TODO
+ * @ author  山羊来了
+ * @ date 2020/3/25---15:35
+ */
+public class App {
+
+    @Test
+    public void test(){
+        System.out.println("hello gradle!!!");
+    }
+
+}
